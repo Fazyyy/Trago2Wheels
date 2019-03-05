d84b928dc6382300285de4329d40d016c9296f5f
Jaimie Warburton <warbz@live.co.uk>
2019-03-05 12:28:36 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Services from "../components/services";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { Helmet } from "react-helmet";
 import Map from "../components/map";
-import homeIntro from "../components/homeIntro";
+import HomeIntro from "../components/homeIntro";
 import Img from "gatsby-image";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -29,7 +29,7 @@ export default ({ data }) => {
           rel="stylesheet"
         />
       </Helmet>
-      <homeIntro />
+      <HomeIntro />
       <SwiperHome />
       <div className="homeIntro gbg">
         <div className="container">
