94ddd0cb8ac3a1f295d35a6e35b1866c5de73696
Jaimie Warburton <warbz@live.co.uk>
2019-03-05 17:07:17 +0000

    voucher display update

--- src/templates/events-post.js
+++ src/templates/events-post.js
@@ -1,103 +1,166 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Layout from "../components/layout"
-import { Helmet } from 'react-helmet'
-import Img from 'gatsby-image'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import { Helmet } from "react-helmet";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
 
-library.add( faCalendarAlt )
+library.add(faCalendarAlt);
 
 export default ({ data }) => {
   const post = data.markdownRemark.frontmatter;
   const titleStyle = {
-      position: 'absolute',
-      bottom: '15px',
-      left: '15px',
-      fontSize: '48px',
-      color: '#fff',
-      zIndex: '3'
-  }
+    position: "absolute",
+    bottom: "15px",
+    left: "15px",
+    fontSize: "48px",
+    color: "#fff",
+    zIndex: "3"
+  };
   const bgStyle = {
     backgroundImage: `url(` + post.featuredImage.publicURL + `)`
-  }
-    return (
+  };
+  return (
     <Layout>
       <Helmet>
         <title>Trago2Wheels - {post.title}</title>
       </Helmet>
-        <div className="container-fluid motorbikeParallax" style={ bgStyle } >
-          <div className="container" style={{ position:'relative', height:'100%' }}>
-            <h1 style={ titleStyle }>{ post.title }</h1>
-          </div>
+      <div className="container-fluid motorbikeParallax" style={bgStyle}>
+        <div
+          className="container"
+          style={{ position: "relative", height: "100%" }}
+        >
+          <h1 style={titleStyle}>{post.title}</h1>
         </div>
-        <div className="container spacing single">
-          <div className="bikeContainer">
-            <div className="row">
-              <div className="col-sm-6">
-                <Img fluid={post.eventImage.childImageSharp.fluid} />              
-              </div>
-              <div className="col-sm-6">
-                <h2>{post.title}</h2>
-                <p><FontAwesomeIcon icon="calendar-alt" /> { post.ezDate }</p>
-                <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
-                <h3 style={{ color: '#000', fontSize: '16px' }}>Sign Up</h3>
-                <p style={{ margin: '0' }}>Stay up to date with the latest news and events by joining our mailing list.</p>
-                <p><small>We promise not to spam you.</small></p>
-                <div id="mc_embed_signup">
-                  <form action="https://trago.us20.list-manage.com/subscribe/post?u=71e927d89bfcabbe8d580dbf2&amp;id=64904a42d0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
-                      <div id="mc_embed_signup_scroll">
-                          <div className="mc-field-group">
-                              <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" placeholder="Email*" />
-                          </div>
-                          <div className="mc-field-group">
-                              <input type="text" name="FNAME" className="" id="mce-FNAME" placeholder="First Name" />
-                          </div>
-                          <div className="mc-field-group">
-                              <input type="text" name="LNAME" className="" id="mce-LNAME" placeholder="Last Name" />
-                          </div>
-                          <div id="mce-responses" className="clear">
-                              <div className="response" id="mce-error-response" style={{display:'none'}}></div>
-                              <div className="response" id="mce-success-response" style={{display:'none'}}></div>
-                          </div>
-                          <div style={{position: 'absolute', left: '-5000px'}} aria-hidden="true">
-                              <input type="text" name="b_71e927d89bfcabbe8d580dbf2_64904a42d0" tabIndex="-1" value="" />
-                          </div>
-                          <div className="clear">
-                              <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" />
-                          </div>
-                      </div>
-                  </form>
-                </div>
+      </div>
+      <div className="container spacing single">
+        <div className="bikeContainer">
+          <div className="row">
+            <div className="col">
+              <h2 style={{ color: "#1c2858" }}>{post.title}</h2>
+              <p
+                style={{
+                  marginBottom: "25px",
+                  borderBottom: "1px solid #1c2858",
+                  paddingBottom: "10px"
+                }}
+              >
+                <FontAwesomeIcon color="#1c2858" icon="calendar-alt" />{" "}
+                {post.ezDate}
+              </p>
+              <div
+                dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
+              />
+              <h3 style={{ color: "#1c2858", fontSize: "22px" }}>Sign Up</h3>
+              <p style={{ margin: "0" }}>
+                Stay up to date with the latest news and events by joining our
+                mailing list.
+              </p>
+              <p>
+                <small>We promise not to spam you.</small>
+              </p>
+              <div id="mc_embed_signup">
+                <form
+                  action="https://trago.us20.list-manage.com/subscribe/post?u=71e927d89bfcabbe8d580dbf2&amp;id=64904a42d0"
+                  method="post"
+                  id="mc-embedded-subscribe-form"
+                  name="mc-embedded-subscribe-form"
+                  className="validate"
+                  target="_blank"
+                  noValidate
+                >
+                  <div id="mc_embed_signup_scroll">
+                    <div className="mc-field-group">
+                      <input
+                        type="email"
+                        name="EMAIL"
+                        className="required email"
+                        id="mce-EMAIL"
+                        placeholder="Email*"
+                      />
+                    </div>
+                    <div className="mc-field-group">
+                      <input
+                        type="text"
+                        name="FNAME"
+                        className=""
+                        id="mce-FNAME"
+                        placeholder="First Name"
+                      />
+                    </div>
+                    <div className="mc-field-group">
+                      <input
+                        type="text"
+                        name="LNAME"
+                        className=""
+                        id="mce-LNAME"
+                        placeholder="Last Name"
+                      />
+                    </div>
+                    <div id="mce-responses" className="clear">
+                      <div
+                        className="response"
+                        id="mce-error-response"
+                        style={{ display: "none" }}
+                      />
+                      <div
+                        className="response"
+                        id="mce-success-response"
+                        style={{ display: "none" }}
+                      />
+                    </div>
+                    <div
+                      style={{ position: "absolute", left: "-5000px" }}
+                      aria-hidden="true"
+                    >
+                      <input
+                        type="text"
+                        name="b_71e927d89bfcabbe8d580dbf2_64904a42d0"
+                        tabIndex="-1"
+                        value=""
+                      />
+                    </div>
+                    <div className="clear">
+                      <input
+                        type="submit"
+                        value="Subscribe"
+                        name="subscribe"
+                        id="mc-embedded-subscribe"
+                        className="button"
+                      />
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
         </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
 export const query = graphql`
-query($slug: String!) {
+  query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
-            html
-            frontmatter {
-              title
-              eventDate
-              ezDate
-              featuredImage {
-                publicURL
-                name
-              }
-              eventImage {
-                childImageSharp {
-                  fluid(maxWidth:600) {
-                    ...GatsbyImageSharpFluid_tracedSVG
-                  }
-                }
-              }
-            }
+      html
+      frontmatter {
+        title
+        eventDate
+        ezDate
+        featuredImage {
+          publicURL
+          name
         }
+        eventImage {
+          childImageSharp {
+            fluid(maxWidth: 600) {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        }
+      }
     }
-`
+  }
+`;
--- src/templates/motorbike-post.js
+++ src/templates/motorbike-post.js
@@ -1,94 +1,113 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Layout from "../components/layout"
-import Img from 'gatsby-image'
-import { Helmet } from 'react-helmet'
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import Img from "gatsby-image";
+import { Helmet } from "react-helmet";
 
 export default ({ data }) => {
   const post = data.markdownRemark.frontmatter;
   const titleStyle = {
-      position: 'absolute',
-      bottom: '15px',
-      left: '15px',
-      fontSize: '48px',
-      color: '#fff',
-      zIndex: '3'
-  }
+    position: "absolute",
+    bottom: "15px",
+    left: "15px",
+    fontSize: "48px",
+    color: "#fff",
+    zIndex: "3"
+  };
   const bgStyle = {
     backgroundImage: `url(` + post.featuredImage.publicURL + `)`
-  }
-  const voucher = (parseFloat(post.oldprice.replace(/,/g, '')) / 10).toFixed(2);
-    return (
+  };
+  const voucher = (parseFloat(post.oldprice.replace(/,/g, "")) / 10).toFixed(2);
+  return (
     <Layout>
       <Helmet>
         <title>Trago2Wheels - {post.title}</title>
       </Helmet>
-        <div className="container-fluid motorbikeParallax" style={bgStyle} >
-          <div className="container" style={{position:'relative', height:'100%'}}>
-            <h1 style={titleStyle}>{post.title}</h1>
-          </div>
+      <div className="container-fluid motorbikeParallax" style={bgStyle}>
+        <div
+          className="container"
+          style={{ position: "relative", height: "100%" }}
+        >
+          <h1 style={titleStyle}>{post.title}</h1>
         </div>
-        <div className="container spacing single">
-          <div className="bikeContainer">
-            <div className="row">
-              <div className="col-sm-6">
-                <Img
-                  fluid={post.mainImage.childImageSharp.fluid}
-                />
-              </div>
-              <div className="col-sm-6">
+      </div>
+      <div className="container spacing single">
+        <div className="bikeContainer">
+          <div className="row">
+            <div className="col-sm-6">
+              <Img fluid={post.mainImage.childImageSharp.fluid} />
+            </div>
+            <div className="col-sm-6">
               <h2>{post.title}</h2>
-              <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
-                <ul className="bikeInfo">
-                  <li>Weight <span>{post.weight}kg</span></li>
-                  <li>Engine <span>{post.Engine}</span></li>
-                  <li>Displacement <span>{post.displacement}cc</span></li>
-                  <li>Max Power <span>{post.maxpower}bhp</span></li>
-                  <li>Transmission <span>{post.transmission}</span></li>
-                  <li>Fuel Tank <span>{post.fueltank} Litres</span></li>
-                </ul>
-                <p style={{
-                  textAlign:'right',
-                  fontWeight: '700',
-                  fontSize: '22px'
-                }}>
-                  &pound;{post.oldprice}
-                </p>
-                <p style={{ textAlign:'right' }}>Voucher: £{voucher}</p>
+              <div
+                dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
+              />
+              <ul className="bikeInfo">
+                <li>
+                  Weight <span>{post.weight}kg</span>
+                </li>
+                <li>
+                  Engine <span>{post.Engine}</span>
+                </li>
+                <li>
+                  Displacement <span>{post.displacement}cc</span>
+                </li>
+                <li>
+                  Max Power <span>{post.maxpower}bhp</span>
+                </li>
+                <li>
+                  Transmission <span>{post.transmission}</span>
+                </li>
+                <li>
+                  Fuel Tank <span>{post.fueltank} Litres</span>
+                </li>
+              </ul>
+              <p
+                style={{
+                  textAlign: "right",
+                  fontWeight: "700",
+                  fontSize: "22px"
+                }}
+              >
+                &pound;{post.oldprice}
+              </p>
+              <div className="container-fluid discountDisp">
+                <p style={{ textAlign: "right" }}>Voucher: £{voucher}</p>
               </div>
             </div>
           </div>
         </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
 export const query = graphql`
-query($slug: String!) {
+  query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
-            html
-            frontmatter {
-              title
-              oldprice
-              newprice
-              maxpower
-              fueltank
-              transmission
-              Engine
-              weight
-              displacement
-              featuredImage {
-                publicURL
-                name
-              }
-              mainImage {
-                childImageSharp {
-                  fluid(maxWidth:600) {
-                    ...GatsbyImageSharpFluid_tracedSVG
-                  }
-                }
-              }
-            }
+      html
+      frontmatter {
+        title
+        oldprice
+        newprice
+        maxpower
+        fueltank
+        transmission
+        Engine
+        weight
+        displacement
+        featuredImage {
+          publicURL
+          name
         }
+        mainImage {
+          childImageSharp {
+            fluid(maxWidth: 600) {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        }
+      }
     }
-`
+  }
+`;
