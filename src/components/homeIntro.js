89d7967a686064c6878264bfc37c73febf721ba8
Jaimie Warburton <warbz@live.co.uk>
2019-03-05 12:37:13 +0000

    styles change

--- src/components/homeIntro.js
+++ src/components/homeIntro.js
@@ -1,40 +1,35 @@
 import React from "react";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faThumbsUp,
-  faAward,
-  faCheck
-} from "@fortawesome/free-solid-svg-icons";
-
-library.add(faThumbsUp, faAward, faCheck);
+import Container from "./homeIntro.module.css";
 
 class homeIntro extends React.Component {
   render() {
     return (
-      <header>
-        <div id="stripes">
-          <span />
-          <span />
-          <span />
-          <span />
-          <span />
-        </div>
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-6">
-              <h1>Trago2Wheels</h1>
-              <p>
-                Here at Trago2Wheels, we are dedicated to helping our customers
-                find the best bike to suit your individual riding ability. At
-                Trago2Wheels, you will find a wide range of bikes from
-                affordable 50cc scooters to 650cc motorbikes. We provide high
-                quality images and reviews of every motorbike on our site to
-                make choosing your next purchase simple.
-              </p>
+      <Container>
+        <header>
+          <div id="stripes">
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-6">
+                <h1>Trago2Wheels</h1>
+                <p>
+                  Here at Trago2Wheels, we are dedicated to helping our
+                  customers find the best bike to suit your individual riding
+                  ability. At Trago2Wheels, you will find a wide range of bikes
+                  from affordable 50cc scooters to 650cc motorbikes. We provide
+                  high quality images and reviews of every motorbike on our site
+                  to make choosing your next purchase simple.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </header>
+        </header>
+      </Container>
     );
   }
 }
